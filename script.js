97aec54c7805750ac14f5c56f3da863f00205c12
hmstrkey <akkaynt123akk1@gmail.com>
2024-08-11 15:49:20 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,212 +1,219 @@
-const appConfigs = {
-    Bike: {
+const games = {
+    1: {
+        name: 'Riding Extreme 3D',
         appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-        promoId: '43e35910-c168-4634-ad4f-52fd764a843f'
+        promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
     },
-    Clone: {
-        appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-        promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767'
-    },
-    Chain: {
+    2: {
+        name: 'Chain Cube 2048',
         appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
-        promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3'
+        promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
     },
-    Train: {
+    3: {
+        name: 'My Clone Army',
+        appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
+        promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
+    },
+    4: {
+        name: 'Train Miner',
         appToken: '82647f43-3f87-402d-88dd-09a90025313f',
-        promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954'
+        promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
     }
 };
 
-let APP_TOKEN = appConfigs.Bike.appToken;
-let PROMO_ID = appConfigs.Bike.promoId;
-const DEBUG_MODE = false;
-const EVENTS_DELAY = DEBUG_MODE ? 350 : 20000;
-const NUM_KEYS_TO_GENERATE = 4;
-let USER_ID = 'test_id';
-let USER = 'test_user';
-let HASH = 'test_hash';
-
-function updateConstants() {
-    const selectedApp = document.getElementById('appSelect').value;
-    APP_TOKEN = appConfigs[selectedApp].appToken;
-    PROMO_ID = appConfigs[selectedApp].promoId;
-}
-
 function generateClientId() {
     const timestamp = Date.now();
-    const randomNumbers = Array.from({ length: 19 }, () => Math.floor(Math.random() * 10)).join('');
-    return `${timestamp}-${randomNumbers}`;
-}
+    const randomNumbers = [];
 
-function generateDeviceId() {
-    const baseDeviceId = '00000000000-0000000000000000000';
-    const randomDigits = Array.from({ length: 5 }, () => Math.floor(Math.random() * 10)).join('');
-    let deviceIdArray = baseDeviceId.split('');
-    for (let i = 0; i < randomDigits.length; i++) {
-        const randomIndex = Math.floor(Math.random() * deviceIdArray.length);
-        deviceIdArray[randomIndex] = randomDigits[i];
-    }
-    return deviceIdArray.join('');
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function commitKey(key) {
-    const keyData = {
-        id: USER_ID,
-        user: USER,
-        hash: HASH,
-        key: key
-    };
-    const keyDataEncoded = btoa(JSON.stringify(keyData));
-    const url = DEBUG_MODE ?
-        `http://localhost:7000/key?v=${keyDataEncoded}` :
-        `http://176.119.159.166:7000/key?v=${keyDataEncoded}`;
-
-    try {
-        const response = await fetch(url, { method: 'POST' });
-        return await response.json();
-    } catch (error) {
-        console.error('Error committing key:', error);
-        return { status: 'error' };
-    }
-}
-
-async function login(clientId) {
-    if (DEBUG_MODE) {
-        return `${APP_TOKEN}:deviceid:${generateDeviceId()}:${Date.now()}`;
+    for (let i = 0; i < 19; i++) {
+        randomNumbers.push(Math.floor(Math.random() * 10));
     }
 
-    try {
-        const response = await fetch('https://api.gamepromo.io/promo/login-client', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                appToken: APP_TOKEN,
-                clientId: clientId,
-                clientOrigin: 'deviceid'
-            })
-        });
-        const data = await response.json();
-        return data.clientToken;
-    } catch (error) {
-        console.error('Error logging in:', error);
-        return null;
-    }
+    return `${timestamp}-${randomNumbers.join('')}`;
 }
 
-async function emulateProgress(clientToken) {
-    if (DEBUG_MODE) {
-        return Math.random() < 0.5;
-    }
 
-    try {
-        const response = await fetch('https://api.gamepromo.io/promo/register-event', {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${clientToken}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                promoId: PROMO_ID,
-                eventId: generateClientId(),
-                eventOrigin: 'undefined'
-            })
-        });
-        const data = await response.json();
-        return data.hasCode;
-    } catch (error) {
-        console.error('Error emulating progress:', error);
-        return false;
-    }
-}
-
-async function generateKey(clientToken) {
-    try {
-        const response = await fetch('https://api.gamepromo.io/promo/create-code', {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${clientToken}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ promoId: PROMO_ID })
-        });
-        const data = await response.json();
-
-        // Логируем полученный ответ, чтобы увидеть весь промокод
-        console.log('Generated promo code response:', data);
-
-        return data.promoCode; // Убедитесь, что весь промокод возвращается
-    } catch (error) {
-        console.error('Error generating key:', error);
-        return null;
-    }
-}
-
-async function generateSingleKey() {
+async function loginClient(gameNumber) {
     const clientId = generateClientId();
-    const clientToken = await login(clientId);
+    const url = 'https://api.gamepromo.io/promo/login-client';
 
-    if (!clientToken) {
-        appendToDisplay('Авторизация не выполнена. Пропуск генерации ключа.');
-        return null;
-    }
+    const data = {
+        appToken: games[gameNumber].appToken,
+        clientId: clientId,
+        clientOrigin: 'deviceid'
+    };
 
-    appendToDisplay('Авторизация выполнена, ожидайте генерации ключа...');
+    const headers = {
+        'Content-Type': 'application/json; charset=utf-8',
+    };
 
-    for (let j = 0; j < 7; j++) {
-        if (await emulateProgress(clientToken)) {
-            break;
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(data)
+        });
+        const result = await response.json();
+
+        if (result.error_code === 'TooManyIpRequest') {
+            console.log('Too many requests');
+            await new Promise(resolve => setTimeout(resolve, 10000));
+            return loginClient(gameNumber);
         }
-        await sleep(EVENTS_DELAY);
+        return result.clientToken;
+    } catch (error) {
+        return loginClient(gameNumber);
     }
+}
 
-    const key = await generateKey(clientToken);
-    if (key) {
-        appendToDisplay(`Generated key: ${key}`);
+async function registerEvent(token, gameNumber) {
+    await new Promise(resolve => setTimeout(resolve, 20000));
+    const eventId = generateRandomUUID();
+    const url = 'https://api.gamepromo.io/promo/register-event';
+    const data = {
+        promoId: games[gameNumber].promoId,
+        eventId: eventId,
+        eventOrigin: 'undefined'
+    };
+    const headers = {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json; charset=utf-8',
+    };
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(data)
+        });
 
-        if (USER_ID) {
-            const result = await commitKey(key);
-            if (result.status === 'ok') {
-                appendToDisplay(`Points: ${result.points}`);
-                return key;
-            } else {
-                appendToDisplay('Ошибка при сохранении ключа.');
-                return null;
-            }
+        const result = await response.json();
+
+        if (!result.hasCode) {
+            console.log('Retry register event');
+            return registerEvent(token, gameNumber);
         } else {
-            return key;
+            return token;
         }
-    } else {
-        appendToDisplay('Ошибка при генерации ключа.');
-        return null;
+    } catch (error) {
+        console.error('Fatal error:', error.message);
+        let newToken = await loginClient(gameNumber);
+        return registerEvent(newToken, gameNumber);
     }
 }
 
-async function findValidKeys(numKeys) {
-    const keyDisplay = document.getElementById('jsonDataDisplay');
-    keyDisplay.style.display = "block";
-    keyDisplay.textContent = ''; // Clear previous content
+async function createCode(token, gameNumber) {
+    let response;
+    do {
+        try {
+            await new Promise(resolve => setTimeout(resolve, 1000))
+            const url = 'https://api.gamepromo.io/promo/create-code';
 
-    const keyPromises = Array.from({ length: numKeys }, () => generateSingleKey());
-    const keys = await Promise.all(keyPromises);
+            const data = {
+                promoId: games[gameNumber].promoId
+            };
 
-    const validKeys = keys.filter(key => key !== null);
-    if (validKeys.length > 0) {
-        appendToDisplay(`Valid keys found: ${validKeys.join(', ')}`);
-    } else {
-        appendToDisplay('No valid keys found.');
+            const headers = {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json; charset=utf-8',
+            };
+            response = await fetch(url, {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify(data)
+            });
+
+            const result = await response.json();
+
+            if (result.promoCode) {
+                return result.promoCode;
+            }
+
+        } catch (error) {
+            console.error('Fatal error:', error.message);
+        }
+    } while (!response || !response.promoCode); // Повторяем запрос, если ничего не возвращает
+}
+
+function generateRandomUUID() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    );
+}
+
+const generateButton = document.getElementById('generateButton');
+const generateTimeValue = document.getElementById('generate-time-value');
+const generateProcessBlock = document.getElementById('process-generate-block');
+let keyBlock = document.getElementById('keys-block');
+
+async function generate() {
+    generateButton.style.display = 'none';
+    const games = document.getElementById('game-names-select');
+    games.disabled = true;
+    generateProcessBlock.style.display = 'flex';
+    const endGenerateTime = Date.now() + 4 * 40 * 1000;
+
+    const selectedGame = parseInt(games.value);
+
+    keyBlock.style.display = 'none';
+
+    generateTimeValue.innerText = '⏳';
+
+    let generateTimeInterval = setInterval(() => startProcessGeneration(endGenerateTime), 1000);
+    const codes = [];
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    const tasks = [];
+
+    for (let i = 0; i < 4; i++) {
+        tasks.push((async (index) => {
+            try {
+                let token = await loginClient(selectedGame);
+                let registerToken = await registerEvent(token, selectedGame);
+                codes[index] = await createCode(registerToken, selectedGame);
+            } catch (error) {
+                codes[index] = `Error: ${error.message}`;
+            }
+        })(i));
     }
+
+    await Promise.all(tasks);
+
+    keyBlock.style.display = 'flex';
+
+    for (let i = 0; i < 4; i++) {
+        let keyValue = document.getElementById('keys-value-' + (i + 1));
+        keyValue.innerText = codes[i];
+    }
+
+    generateButton.style.display = 'block';
+    games.disabled = false;
+    clearInterval(generateTimeInterval);
+    generateProcessBlock.style.display = 'none';
+    generateTimeValue.innerText = '👌';
+    console.log(codes);
 }
 
-function appendToDisplay(text) {
-    const keyDisplay = document.getElementById('jsonDataDisplay');
-    keyDisplay.textContent += text + '\n';
-    keyDisplay.scrollTop = keyDisplay.scrollHeight; // Auto-scroll to bottom
+function startProcessGeneration(generationTime) {
+    function updateProcessGenerationTime() {
+        const now = new Date();
+        const distance = generationTime - now.getTime();
+
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        generateTimeValue.innerText = '≈ ' +
+            String(hours).padStart(2, '0') + ':' +
+            String(minutes).padStart(2, '0') + ':' +
+            String(seconds).padStart(2, '0');
+
+        if (distance < 0) {
+            generateTimeValue.innerText = "⏳";
+        }
+    }
+
+    updateProcessGenerationTime();
 }
 
-function generateKeys() {
-    findValidKeys(NUM_KEYS_TO_GENERATE);
-}
+generateButton.addEventListener('click', generate);
