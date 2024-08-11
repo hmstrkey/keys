d07399a2ad2437bf625aab6bed75f3412ddc4685
hmstrkey <akkaynt123akk1@gmail.com>
2024-08-11 16:19:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -220,13 +220,13 @@ function startProcessGeneration(generationTime) {
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        generateTimeValue.innerText = '≈ ' +
+        generateTimeValue.innerText = '' +
             String(hours).padStart(2, '0') + ':' +
             String(minutes).padStart(2, '0') + ':' +
             String(seconds).padStart(2, '0');
 
         if (distance < 0) {
-            generateTimeValue.innerText = "⏳";
+            generateTimeValue.innerText = "Ожидайте...";
         }
     }
 
